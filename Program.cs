38ad82f89cc9300c83c3e77ea8d92867f00e9a36
LuayRahil02 <54432462+LuayRahil02@users.noreply.github.com>
2019-09-13 09:32:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace talet_mellan_1_och_100_LUAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomerare = new Random();
+            int hemligt_nummer = randomerare.Next(1, 101);
+
+            Console.WriteLine("Gissa ett tal mellan 1 och 100!");
+            string str = Console.ReadLine();
+            int tal = Convert.ToInt32(str);
+            bool loop = true;
+            int f = 0;
+
+            while (loop == true)
+
+            {
+                f++;
+                if (tal < hemligt_nummer)
+                {
+                    Console.WriteLine("Gissa på större tal!");
+                    str = Console.ReadLine();
+                    tal = Convert.ToInt32(str);
+                }
+                else if (tal > hemligt_nummer)
+                {
+                    Console.WriteLine("Gissa på ett mindre tal!");
+                    str = Console.ReadLine();
+                    tal = Convert.ToInt32(str);
+                }
+
+                else if (tal == hemligt_nummer)
+
+                {
+                    Console.WriteLine("Bra gissat!");
+                    Console.WriteLine("Du gjorde det på " + f + " försök");
+                    loop = false;
+
+                }
+
+            }
+
+        }
+    }
+}
